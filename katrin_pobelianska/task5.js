d49331cc87db8b2f514e9499572f0b8f99b77cb0
Katrin <alieva957@gmail.com>
2024-02-14 15:11:00 +0100

    update to task5.js

--- katrin_pobelianska/task5.js
+++ katrin_pobelianska/task5.js
@@ -1,8 +1,24 @@
+// old school
 db.txs.aggregate([
     {
         $group: {
             _id: null,
             total_txs: { $sum: 1 }
+        }
+    }
+])
+
+// the shortest code
+db.txs.aggregate([
+    { $count: 'total_txs' }
+])
+
+// modern decision
+db.txs.aggregate([
+    {
+        $group: {
+            _id: null,
+            'total_txs': { $count: {} }
         }
     }
 ])
